6a1857daaf04aee58d1030017ff59b76f4e05a12
bitwise-rude <080bel042.meyan@pcampus.edu.np>
2025-05-24 13:10:14 +0545

    Update app.py

--- app.py
+++ app.py
@@ -307,5 +307,7 @@ def rejectDebtPay():
     
     return {"success": False, "message": "No debt payment request found"}
 
+
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
